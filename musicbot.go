2d14d20ea103e130f00b77f3b989a2605bff7226
Leonardo Gago <ljgago@gmail.com>
2016-11-23 16:02:55 -0300

    Remove code without use

--- musicbot.go
+++ musicbot.go
@@ -9,15 +9,6 @@ import (
   "github.com/bwmarrin/discordgo"
   "github.com/spf13/viper"
   "github.com/fsnotify/fsnotify"
-  //"reflect"
-  //"github.com/BurntSushi/toml"
-)
-
-const (
-  channels  int = 2                   // 1 for mono, 2 for stereo
-  frameRate int = 48000               // audio sampling rate
-  frameSize int = 960                 // uint16 size of each audio frame
-  //maxBytes  int = (frameSize * 2) * 2 // max size of opus data
 )
 
 var bot map[string]string
